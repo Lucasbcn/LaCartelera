86b58dad14e39af92dec55606369c0ebc6a17218
Lucas Abidoral <lucasabidoral43@gmail.com>
2022-03-24 11:34:41 +0100

    hola

--- Index.js/index.js
+++ Index.js/index.js
@@ -1,23 +1,30 @@
-const requestURL = '../json/index.json'
+const requestURL = './json/peliculas.json';
     
 async function fetchMoviesJSON(){
     const response = await fetch(requestURL);
-    const movies = await response.json();
-    return movies;
+    const peliculas = await response.json();
+    return peliculas;
     }
 
-    fetchMoviesJSON().then(movies => {
-        console.log(movies)
+    fetchMoviesJSON().then(peliculas => {
 
-    for(let index = 0; index < movies.peliculas.lenght; index++) {
+
+    for(let index = 0; index < peliculas.peliculas.length; index++) 
+      {
        
-        let title = movies.peliculas[index].nombre;
-        let director = movies.peliculas[index].director
+        let name = peliculas.peliculas[index].name;
+        let img = peliculas.peliculas[index].img;
+        let title = peliculas.peliculas[index].title;
+        let clasification = peliculas.peliculas[index].clasificacion;
+        let director= peliculas.peliculas[index].director;
         
-        moviesSection.innerHTML += `
-            <div class="card" style="width: 18rem;">
+        section.innerHTML += `
+            <div class="card" style="width: 10rem;">
+                <img class="card-img-top" src="${img}" alt="Card image cap">
                 <div class="card-body">
-                    <h5 class="card-title">${title}</h5>
+                    <h5 class="card-title">${name}</h5>
+                    <p class="card-text">${director}</p>
+                    <p class="card-text">${clasification}</p>  
                 </div>
             </div>
         `    
